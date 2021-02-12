21db0523e52e55d0761c3e22e5c3d7602f6b5362
osha7 <ogroetz@gmail.com>
2021-02-12 13:28:52 -0700

    LinkedList and prepend

--- LinkedList.js
+++ LinkedList.js
@@ -1,1 +1,38 @@
 // define linked lists 
+
+class LinkedList {
+    constructor() {
+            // ^^ no parameters - no initial values, defaults to empty linked list
+        this.head = null // bc have no head yet
+        // ^^ beginning of our linked list
+        this.length = 0
+        // ^^ so we can keep track of our list length as we add or remove elements (nodes)
+    }
+
+    // insertAtHead(data) {
+    prepend(value) {    
+        // add elements to beginning of list
+
+        //take current head of array and replace it 
+        //need new LinkedList that will reference old head
+        //create new node:  pass it value and the next element in our array
+        const newNode = new LinkedListNode(value, this.head)
+
+        //take current head and set it to node that was just created:
+        this.head = newNode
+
+        //add 1 to length because we added a node:
+        this.length++
+
+    }
+}
+
+class LinkedListNode {
+    // for each one of our elements (nodes) & reference to next node
+    constructor(value, next) {
+        this.value = value
+        this.next = next
+    }
+}
+
+module.exports = LinkedList
--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 // write code to work with Linked List, and print it out
 // to do manual testing
+
+const LinkedList = require('./LinkedList')
+
+// create new linkedlist: (starts empty)
+const ll = new LinkedList()
+// console.log(ll)
+
+//  ===> return value:
+// LinkedList { head: null, length: 0 }
+
+ll.prepend(10) // add 1 element(node) with value of 10
+
+// console.log(ll)
+//  ===> return value:
+// LinkedList {
+//     head: LinkedListNode { value: 10, next: null },
+//     length: 1
+// }
+
+ll.prepend(20) // add 2nd element(node) with value of 20
+
+console.log(ll)
+
+//  ===> return value:
+// LinkedList {
+//     head: LinkedListNode {
+//       value: 20,
+//       next: LinkedListNode { value: 10, next: null } //node with next value of null is the Tail
+//     },
+//     length: 2
+//   }
